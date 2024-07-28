0de7a995c220e0512bf9e0f88b0f9b45c64e5255
ridhilakhina77 <ridhilakhina@gmail.com>
2024-07-28 19:04:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
+
+function draw() {
+  background(5, 0, 0 );
+
+  rotateX(frameCount * 0.05);
+  stroke('red');
+  strokeWeight(20);
+
+  point(-50, -50, 50);
+
+point(-90,-90,90)
+  point(-70,-70,70)
+  point(50, 40 ,50)
+  point(60, 70, 70)
+  point(20, 30, 50);
+  
+//  square(70, 20, 20, 40);
+ // triangle(209,100,350,20,40,50)
+ // circle(20, 30, 100)
+    rotateY(frameCount * 1.10);
+stroke('purple')
+  frameRate(150)
+  
+  point(20,100,100);
+   strokeWeight(9);
+  stroke('turquoise');
+
+   quad(-30, -30, 0, 300, -30, 0, 30, 30, 20, -30, 30, -20);
+
+}
